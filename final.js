b0e4353d905fbf524018720e58f08ef43dc8815b
Ales <ales13992@gmail.com>
2020-07-04 15:41:33 +0300

    Update final.js

--- final.js
+++ final.js
@@ -8,21 +8,19 @@ let startFlag = 0;
 let counter = 0;
 let flagPoint = 0;
 let dividingByZero = false;
-
 let holder = [];
 
 
 const Status = {
-  first1: 0,
-  second1: 0,
+  first: 0,
+  second: 0,
   rest: 0,
-  op1: null,
-  prevOp1: null,
-  restOp1: 0,
-  flag1:0,
-  typeOp1: null,
-  last: null,
-  prevTypeOp: null
+  op: null,
+  prevOp: null,
+  restOp: 0,
+  typeOp: null,
+  prevTypeOp: null,
+  last: null
 };
 
 let CurrentStatus = Object.create(Status);
@@ -49,37 +47,24 @@ for (let i = 0; buttonList.length > i; i++) {
   }
 };
 
+// Turns screen on
 function open(){
   theScreen.style.backgroundColor = "#ffff80";
   theScreen.style.boxShadow = "0px 0px 20px #ffffcc";
-}
-
+};
+// Pushing copy of a current object into array
 function hold(){
   let inside = JSON.parse(JSON.stringify(CurrentStatus));
   holder.push(inside);
-}
-
+};
+// Last element getter
 function lastSymbol(){
   let text = theScreen.innerHTML;
   let splitted = text.split('');
   CurrentStatus.last = splitted[splitted.length-1];
   return CurrentStatus.last;
-}
-
-function delete_last(){
-  let str = theScreen.innerHTML;
-  theScreen.innerHTML = str.slice(0,str.length-1);
-  CurrentStatus.last = str[str.length-1];
-  holder.pop();
-  if (holder[holder.length-1] != undefined) {
-    CurrentStatus = JSON.parse(JSON.stringify(holder[holder.length-1]))
-  }
-  else {
-    reset();
-  }
-  counter = theScreen.innerHTML.length;
-}
-
+};
+// Checking if last element is an operator
 function checkLastElement(lastElem){
   if (lastElem == "+" || lastElem == "-" || lastElem == "x" || lastElem == "/") {
     return true;
@@ -87,8 +72,8 @@ function checkLastElement(lastElem){
   else {
     return false;
   }
-}
-
+};
+// Checking if operator is simple, or super
 function checkOpType(noTypeOp){
   if (noTypeOp == "x" || noTypeOp == "/") {
     return true;
@@ -97,64 +82,37 @@ function checkOpType(noTypeOp){
     return false;
   }
 };
-
+// Searching for specific element
 function checkForElement(char){
-  for (let i = 0; i < CurrentStatus.first1.length; i++) {
-      if (CurrentStatus.first1[i] == char) {
+  for (let i = 0; i < CurrentStatus.first.length; i++) {
+      if (CurrentStatus.first[i] == char) {
         return true;
       }
   }
-        return false;
+      return false;
 };
-
+// Reset function
 function resetValues() {
-  CurrentStatus.first1 = 0;
-  CurrentStatus.second1 = 0;
-  CurrentStatus.rest1 = 0;
-  CurrentStatus.restOp1 = 0;
-  CurrentStatus.op1 = null;
-  CurrentStatus.prevOp1 = null;
-  CurrentStatus.typeOp1 = null;
+  CurrentStatus.first = 0;
+  CurrentStatus.second = 0;
+  CurrentStatus.rest = 0;
+  CurrentStatus.restOp = 0;
+  CurrentStatus.op = null;
+  CurrentStatus.prevOp = null;
+  CurrentStatus.typeOp = null;
   CurrentStatus.last = null;
 
   startFlag = 0;
   holder = [];
 };
+// Divide by zero error
+function zeroDividing(){
+  delete_last();
+  counter = CurrentStatus.first.length;
+  dividingByZero = false;
+};
 
-function reset() {
-  resetValues();
-  theScreen.innerHTML = null;
-}
-
-function point(){
-
-  if (flagPoint == 1){
-      alert("Only one point");
-  }
-  else {
-    if (lastSymbol() == undefined) {
-        CurrentStatus.first1 += this.innerHTML;
-        theScreen.innerHTML += CurrentStatus.first1;
-        open();
-    }
-    else if (checkLastElement(lastSymbol())) {      //if last was an operator ---> add zero firstly
-        CurrentStatus.second1 += this.innerHTML;
-        theScreen.innerHTML += CurrentStatus.second1;
-    }
-    else if (typeof(CurrentStatus.second1) != "string") {
-        CurrentStatus.first1 += this.innerHTML;
-        theScreen.innerHTML += this.innerHTML;
-    }
-    else {
-        CurrentStatus.second1 += this.innerHTML;
-        theScreen.innerHTML += this.innerHTML;
-    }
-    flagPoint = 1;
-    hold();
-    lastSymbol();
-  }
-}
-
+// Font changing depending on amount of elements
 function limit(){
   if (counter < 9) {
       theScreen.style.fontSize = "3em";
@@ -174,13 +132,21 @@ function limit(){
   }
 };
 
+// Main functions attached to html buttons:
+
+// Restart
+function reset() {
+  resetValues();
+  theScreen.innerHTML = null;
+};
+// Show each element on screen by pressing buttons
 function show() {
   theScreen.innerHTML += this.innerHTML;
   if (startFlag != 0) {
-        CurrentStatus.second1 += this.innerHTML;
+        CurrentStatus.second += this.innerHTML;
   }
   else {
-        CurrentStatus.first1 += this.innerHTML;
+        CurrentStatus.first += this.innerHTML;
         open();
   }
   lastSymbol();
@@ -188,129 +154,169 @@ function show() {
   counter++;
   limit();
 };
-
+// Back arrow function
+function delete_last(){
+  let str = theScreen.innerHTML;
+  theScreen.innerHTML = str.slice(0,str.length-1);
+  CurrentStatus.last = str[str.length-1];
+  holder.pop();
+  if (holder[holder.length-1] != undefined) {
+    CurrentStatus = JSON.parse(JSON.stringify(holder[holder.length-1]))
+  }
+  else {
+    reset();
+  }
+  counter = theScreen.innerHTML.length;
+};
+// Operator function
+// Most important function that changes the logic of calculating flowchart and Object values
 function operator() {
   if (checkLastElement(lastSymbol()) || lastSymbol() == ".") {
       delete_last();
   }
   if (lastSymbol() == undefined){
     if (this.innerHTML != "-"){
-        theScreen.innerHTML += CurrentStatus.first1;
+        theScreen.innerHTML += CurrentStatus.first;
     }
     open();
   }
 
-  CurrentStatus.first1 *= 1;
-  CurrentStatus.op1 = this.innerHTML;
+  CurrentStatus.first *= 1;
+  CurrentStatus.op = this.innerHTML;
 
-    if (CurrentStatus.prevOp1 != null) { // previous operator ISN'T null
-        CurrentStatus.typeOp1 = checkOpType(CurrentStatus.op1);
-        CurrentStatus.prevTypeOp = checkOpType(CurrentStatus.prevOp1);
-        operation(CurrentStatus.typeOp1);
+    if (CurrentStatus.prevOp != null) { // previous operator ISN'T null
+        CurrentStatus.typeOp = checkOpType(CurrentStatus.op);
+        CurrentStatus.prevTypeOp = checkOpType(CurrentStatus.prevOp);
+        operation(CurrentStatus.typeOp);
         theScreen.innerHTML += this.innerHTML;
-        CurrentStatus.prevOp1 = CurrentStatus.op1;
+        CurrentStatus.prevOp = CurrentStatus.op;
     }
     else {    // previous operator IS null
         startFlag = 1;
         flagPoint = 0;
-        CurrentStatus.typeOp1 = checkOpType(CurrentStatus.op1);
+        CurrentStatus.typeOp = checkOpType(CurrentStatus.op);
         theScreen.innerHTML += this.innerHTML;
-        CurrentStatus.prevOp1 = CurrentStatus.op1;
+        CurrentStatus.prevOp = CurrentStatus.op;
     }
     hold();
     lastSymbol();
     limit();
     counter++;
 };
+// Changes by pressing a point
+function point(){
 
-function operation(par){
-  if (par == true) {                  // if current operator is super
-    if (CurrentStatus.prevTypeOp == true) { // if previous operator is super
-        calculate(CurrentStatus.prevOp1);
-    }
-    else {                            // if previous operator is simple
-        CurrentStatus.rest1 = CurrentStatus.first1;
-        CurrentStatus.restOp1 = CurrentStatus.prevOp1;
-        CurrentStatus.first1 = CurrentStatus.second1;
-        CurrentStatus.typeOp1 = true;
-    }
-  }
-  else {                              // type of current operator is simple
-    //if (current.prevTypeOp == false) {
-      calculate(CurrentStatus.prevOp1);
-      restOperation();
-  }
-  CurrentStatus.second1 = 0;
-  flagPoint = 0;
-};
-/**
- * Addind or subscribing the rest
- * @param {string} o current operator
- */
-function calculate(o) {
-  if (o == "x")
-  {
-    CurrentStatus.first1 *= +CurrentStatus.second1;
-  }
-  else if (o == "/") {
-    if (CurrentStatus.second1 == 0) {
-        dividingByZero = true;
-        alert("Dividing by zero");
-    }
-    else {
-        CurrentStatus.first1 /= +CurrentStatus.second1;
-    }
-  }
-  else if (o == "+") {
-      CurrentStatus.first1 += +CurrentStatus.second1;
+  if (flagPoint == 1){
+      alert("Only one point");
   }
   else {
-      CurrentStatus.first1 = CurrentStatus.first1 - +CurrentStatus.second1;
-  }
-};
-/**
- * Addind or subscribing the rest
- */
-function restOperation(){
-    if (CurrentStatus.restOp1 == "-") {
-        CurrentStatus.first1 = CurrentStatus.rest1 - CurrentStatus.first1;
+    if (lastSymbol() == undefined) {
+        CurrentStatus.first += this.innerHTML;
+        theScreen.innerHTML += CurrentStatus.first;
+        open();
+    }
+    else if (checkLastElement(lastSymbol())) {      //if last was an operator ---> add zero firstly
+        CurrentStatus.second += this.innerHTML;
+        theScreen.innerHTML += CurrentStatus.second;
+    }
+    else if (typeof(CurrentStatus.second) != "string") {
+        CurrentStatus.first += this.innerHTML;
+        theScreen.innerHTML += this.innerHTML;
     }
     else {
-        CurrentStatus.first1 += CurrentStatus.rest1;
+        CurrentStatus.second += this.innerHTML;
+        theScreen.innerHTML += this.innerHTML;
     }
-    CurrentStatus.restOp1 = null;
-    CurrentStatus.rest1 = 0;
-}
-
-function zeroDividing(){
-  delete_last();
-  counter = CurrentStatus.first1.length;
-  dividingByZero = false;
-}
-
+    flagPoint = 1;
+    hold();
+    lastSymbol();
+  }
+};
+// Final computing and a result
 function equal() {
   if (lastSymbol() == "+" || lastSymbol() == "-"){
-      CurrentStatus.second1 = 0;
+      CurrentStatus.second = 0;
   }
   else if (lastSymbol() == "x" || lastSymbol() == "/") {
-      CurrentStatus.second1 = 1;
+      CurrentStatus.second = 1;
   }
-  calculate(CurrentStatus.op1);
+  calculate(CurrentStatus.op);
   if (dividingByZero == true) {
         return zeroDividing();
   }
   restOperation();
-  CurrentStatus.prevOp1 = CurrentStatus.op1;
-  theScreen.innerHTML = CurrentStatus.first1;
+  CurrentStatus.prevOp = CurrentStatus.op;
+  theScreen.innerHTML = CurrentStatus.first;
   resetValues();
-  CurrentStatus.first1 = theScreen.innerHTML;
+  CurrentStatus.first = theScreen.innerHTML;
   if(checkForElement(".")) {
     flagPoint = 1;
   }
   else {
     flagPoint = 0;
   }
-  counter = CurrentStatus.first1.length;
+  counter = CurrentStatus.first.length;
   limit();
   hold();
+};
+
+// Operator function assistance
+function operation(par){
+  if (par == true) {                  // if current operator is super
+    if (CurrentStatus.prevTypeOp == true) { // if previous operator is super
+        calculate(CurrentStatus.prevOp);
+    }
+    else {                            // if previous operator is simple
+        CurrentStatus.rest = CurrentStatus.first;
+        CurrentStatus.restOp = CurrentStatus.prevOp;
+        CurrentStatus.first = CurrentStatus.second;
+        CurrentStatus.typeOp = true;
+    }
+  }
+  else {                              // type of current operator is simple
+    //if (current.prevTypeOp == false) {
+      calculate(CurrentStatus.prevOp);
+      restOperation();
+  }
+  CurrentStatus.second = 0;
+  flagPoint = 0;
+};
+
+/**
+ * Function that makes a math
+ * @param {string} o current operator
+ */
+function calculate(o) {
+  if (o == "x")
+  {
+    CurrentStatus.first *= +CurrentStatus.second;
+  }
+  else if (o == "/") {
+    if (CurrentStatus.second == 0) {
+        dividingByZero = true;
+        alert("Dividing by zero");
+    }
+    else {
+        CurrentStatus.first /= +CurrentStatus.second;
+    }
+  }
+  else if (o == "+") {
+      CurrentStatus.first += +CurrentStatus.second;
+  }
+  else {
+      CurrentStatus.first = CurrentStatus.first - +CurrentStatus.second;
+  }
+};
+/**
+ * Calculate function assistance
+ */
+function restOperation(){
+    if (CurrentStatus.restOp == "-") {
+        CurrentStatus.first = CurrentStatus.rest - CurrentStatus.first;
+    }
+    else {
+        CurrentStatus.first += CurrentStatus.rest;
+    }
+    CurrentStatus.restOp = null;
+    CurrentStatus.rest = 0;
 };
